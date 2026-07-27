5fdee5798efa51ffadcdd0d47306a341d2f54b52
Ruben van Voorden <rubenvanvoorden@me.com>
2026-07-27 19:49:33 +0200

    remove appending "/0" from some entries in terrain.py
    The "/0" where there because I was testing with a resourcepack that has some
    textures stored in folders with numbered files, for example "0.png".
    Turns out not all resourcepacks do this which is a compatibility concern.

--- mctexturepackconverter/terrain.py
+++ mctexturepackconverter/terrain.py
@@ -3,10 +3,10 @@ from layout import Layout
 
 terrain = Layout(
     layout = [
-        ['grass_block_top', 'stone', 'dirt', 'grass_block_side', 'oak_planks', 'smooth_stone_slab_side', 'smooth_stone', 'bricks', 'tnt_side', 'tnt_top', 'tnt_bottom', 'cobweb', 'plants/poppy/0', 'plants/dandelion/0', '', 'oak_sapling'],
+        ['grass_block_top', 'stone', 'dirt', 'grass_block_side', 'oak_planks', 'smooth_stone_slab_side', 'smooth_stone', 'bricks', 'tnt_side', 'tnt_top', 'tnt_bottom', 'cobweb', 'plants/poppy', 'plants/dandelion', '', 'oak_sapling'],
         ['cobblestone', 'bedrock', 'sand', 'gravel', 'oak_log', 'oak_log_top', 'iron_block', 'gold_block', 'diamond_block', '', '', '', 'red_mushroom', 'brown_mushroom'],
         ['gold_ore', 'iron_ore', 'coal_ore', 'bookshelf', 'mossy_cobblestone', 'obsidian', 'grass_block_side_overlay', 'grass', 'grass_block_top', '', '', 'crafting_table_top', 'furnace_front', 'furnace_side', 'dispenser_front'],
-        ['sponge', 'glass', 'diamond_ore', 'redstone_ore', 'leaves/oak_leaves', '', '', 'plants/dead_bush/0', 'fern', '', '', 'crafting_table_front', 'crafting_table_side', '', 'furnace_top', 'spruce_sapling'],
+        ['sponge', 'glass', 'diamond_ore', 'redstone_ore', 'leaves/oak_leaves', '', '', 'plants/dead_bush', 'fern', '', '', 'crafting_table_front', 'crafting_table_side', '', 'furnace_top', 'spruce_sapling'],
         ['white_wool', 'spawner', 'snow', 'ice', 'grass_block_snow', 'cactus_top', 'cactus_side', 'cactus_bottom', 'clay', 'sugar_cane', 'note_block', 'jukebox_top', '', '', '', 'birch_sapling'],
         ['torch', 'doors/oak_door_top', 'doors/iron_door_top', 'ladder', 'trapdoors/oak_trapdoor', '', 'farmland_moist', 'farmland', 'wheat/wheat_stage0', 'wheat/wheat_stage1', 'wheat/wheat_stage2', 'wheat/wheat_stage3', 'wheat/wheat_stage4', 'wheat/wheat_stage5', 'wheat/wheat_stage6', 'wheat/wheat_stage7'],
         ['', 'doors/oak_door_bottom', 'doors/iron_door_bottom', 'redstone_torch', '', '', 'pumpkin_top', 'netherrack', 'soul_sand', 'glowstone', 'piston_top_sticky', 'piston_top', 'piston_side', 'piston_bottom', 'piston_inner', ''],
